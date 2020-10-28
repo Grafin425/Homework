a8db7aa017fa04eda47bc6ea1989e73f306b596c
Grafin425 <clavik.edin@gmail.com>
2020-10-28 13:54:58 +0200

    js.hw-1

---
+++ JS/homework-1/script.js
@@ -0,0 +1,250 @@
+//task 1
+// let hello = "hello";
+// let owu= 'owu';
+// let com='com';
+// let ua= 'ua';
+// let namber1=1;
+// let namber2=10;
+// let namber3=-990;
+// let namber4=123;
+// let namber5=3.14;
+// let namber6=2.7;
+// let namber7=16;
+// let bool=true;
+// let bool2=false;
+//
+//
+// document.write(hello,owu,com,ua,namber1,namber2,namber3,namber4,namber5,namber6,namber7,bool,bool2)
+// console.log(hello,owu,com,ua,namber1,namber2,namber3,namber4,namber5,namber6,namber7,bool,bool2);
+// // alert(hello);
+// // alert(owu);
+// // alert(com);
+// // alert(ua);
+// // alert(namber1);
+// // alert(namber2);
+// // alert(namber3);
+// // alert(namber4);
+// // alert(namber5);
+// // alert(namber6);
+// // alert(namber7);
+// // alert(bool);
+// // alert(bool2);
+// document.write('<hr>');
+
+//task2
+// hello = "hi!!";
+// owu= 'Okten';
+// com='Web';
+// ua= 'io';
+// namber1=2;
+// namber2=21;
+// namber3=-190;
+// namber4=31;
+// namber5=3.234;
+// namber6=2.1;
+// namber7=26;
+// bool=false;
+// bool2=true;
+//
+// document.write(hello,owu,com,ua,namber1,namber2,namber3,namber4,namber5,namber6,namber7,bool,bool2)
+// console.log(hello,owu,com,ua,namber1,namber2,namber3,namber4,namber5,namber6,namber7,bool,bool2);
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(namber1);
+// alert(namber2);
+// alert(namber3);
+// alert(namber4);
+// alert(namber5);
+// alert(namber6);
+// alert(namber7);
+// alert(bool);
+// alert(bool2);
+// document.write('<hr>');
+
+//task3
+//
+// const constNam1=23;
+// const constNam2=12;
+// const constNam3=32;
+// const constString1='hello';
+// const constString2='world';
+// const constString3='!!!';
+//
+// document.write(constNam1,constNam2,constNam3,constString1,constString2,constString3);
+// console.log(constNam1,constNam2,constNam3,constString1,constString2,constString3);
+// alert(constNam1);
+// alert(constNam2);
+// alert(constNam3);
+// alert(constString1)
+// alert(constString2)
+// alert(constString3)
+
+//task4
+// let name = prompt("is your name?" );
+// console.log(name);
+// alert(name);
+// document.write(name);
+// let surname = prompt("is your Surname?" );
+// console.log(surname);
+// alert(surname);
+// document.write(surname);
+// let middleName = prompt("is your middle name?" );
+// console.log(middleName);
+// alert(middleName);
+// document.write(middleName);
+
+//task5
+//  let person =(name+surname+middleName)
+//  console.log(person);
+
+// task6
+// let momName = prompt("is your name?" );
+// console.log(momName);
+// alert(momName);
+// document.write(momName);
+// let momSurname = prompt("is your Surname?" );
+// console.log(momSurname);
+// alert(momSurname);
+// document.write(momSurname);
+// let momMiddleName = prompt("is your middle name?" );
+// console.log(momMiddleName);
+// alert(momMiddleName);
+// document.write(momMiddleName);
+// let momPerson =(momName+momSurname+momMiddleName)
+// console.log(momPerson);
+//
+// let dadName = prompt("is your name?" );
+// console.log(dadName);
+// alert(dadName);
+// document.write(dadName);
+// let dadSurname = prompt("is your Surname?" );
+// console.log(dadSurname);
+// alert(dadSurname);
+// document.write(dadSurname);
+// let dadMiddleName = prompt("is your middle name?" );
+// console.log(dadMiddleName);
+// alert(dadMiddleName);
+// document.write(dadMiddleName);
+// let dadPerson =(dadName+dadSurname+dadMiddleName)
+// console.log(dadPerson);
+//
+// let brotherName = prompt("is your name?" );
+// console.log(brotherName);
+// alert(brotherName);
+// document.write(brotherName);
+// let brotherSurname = prompt("is your Surname?" );
+// console.log(brotherSurname);
+// alert(brotherSurname);
+// document.write(brotherSurname);
+// let brotherMiddleName = prompt("is your middle name?" );
+// console.log(brotherMiddleName);
+// alert(brotherMiddleName);
+// document.write(brotherMiddleName);
+// let brotherPerson =(brotherName+brotherSurname+brotherMiddleName)
+// console.log(brotherPerson);
+
+
+//task7
+// let number1= prompt('enter the number 1\n');
+// let number2= prompt('enter the number 2\n')
+// let number3= prompt('enter the number 3\n')
+//
+// console.log(+number1,+number2,+number3);
+
+//task8
+//
+// let number1=parseInt(prompt('enter the number 1\n'));
+// let number2= parseInt(prompt('enter the number 2\n'));
+// let number3= parseInt(prompt('enter the number 3\n'));
+// let number4= parseInt(prompt('enter the number 4\n'));
+//
+// result=number1+number2+number3+number4;
+// console.log(result);
+
+//task9
+//
+// let number1=parseFloat(prompt('enter the number 1\n'));
+// let number2= parseFloat(prompt('enter the number 2\n'));
+// let number3= parseFloat(prompt('enter the number 3\n'));
+//
+// result=number1+number2+number3;
+// console.log(result);
+
+//task10
+//
+// let number1= Math.round(parseFloat(prompt('enter the number 1\n')));
+// let number2= Math.round(parseFloat(prompt('enter the number 2\n')));
+// let number3= Math.round(parseFloat(prompt('enter the number 3\n')));
+//
+//
+// console.log(number1)
+// result=number1+number2+number3;
+// console.log(result);
+
+//task11
+// let number1=parseInt(prompt('enter the number \n'));
+// let number2= parseInt(prompt('enter the pow\n'));
+// result= Math.pow(number1,number2);
+// console.log(result)
+
+//task12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined
+// console.log(typeof a ,typeof b, typeof c ,typeof d);
+
+//task13
+// let bool= (5<6);
+//
+// if ( bool){
+//     console.log("Правда");
+// } else console.log("Не правда");
+// bool=(5>6);
+// if ( bool){
+//     console.log("Правда");
+// } else {console.log("Не правда");}
+// bool=(5==6);
+// if (bool){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// bool=5>=6;
+// if (bool){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// //
+// let bool2=(10==10);
+// if (bool2){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// bool2=(10===10);
+// if (bool2){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// //
+// let bool3=(10!==10);
+// if (bool3){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// bool3=(10!=10);
+// if (bool3){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// bool3=(10>10);
+// if (bool3){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// //
+// let bool4=(123==="123");
+// if (bool4){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+// bool4=(123!=="123");
+// if (bool4){
+//     console.log("Правда");
+// } else { console.log("Не правда");}
+
+
+
