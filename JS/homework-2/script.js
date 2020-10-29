749c2cbc1fae47bb42947572fa56d0de97ab025b
Grafin425 <clavik.edin@gmail.com>
2020-10-29 23:44:53 +0200

    js.hw-2

---
+++ JS/homework-2/script.js
@@ -0,0 +1,140 @@
+//task1-4
+// let numbers=[23,43,12,32,9];
+// let string=['hi','okten', 'how a you', 'bi-bi','!!!' ]
+// let random=['che','string','znachenya','tyt','5!',1,2,0,43,8,true,false,true,true,false]
+// console.log(string);
+// console.log(numbers);
+// console.log(random);
+
+//task5
+// let array=[];
+// array[0]='task2';
+// array[1]=23;
+// array[2]=65;
+// array[3]=-12;
+// array[4]=true
+// array[5]='box';
+// console.log(array)
+
+//task7-8
+// for( let i=0;i<10;i++){
+//     document.write('<h1>Hello World!!</h1>');
+// }
+// for( let i=0;i<10;i++){
+//     document.write(`<h1>Hello World!!${i}</h1>`);
+// }
+
+//task9-10
+// let start=0;
+// while (start<=20){
+//     start++
+//     document.write('<h1>Hi okten!</h1>')
+// }
+//
+// let start1=0;
+// while (start1<=20){
+//     start1++
+//     document.write(`<h1>Hi okten! ${start1}</h1>`)
+// }
+
+//task11
+// let array=[1,2,5,6,2,1,4,134,34,1]
+// for(let i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
+// let arrayStr=['try','bool','blue','red','beteer','floo','all','macdak','gooo','stop' ];
+// for( let i=0;i<arrayStr.length;i++){
+//     console.log(arrayStr[i]);
+// }
+//
+// let arrayRandom=['try',32,true,98,false,-12,true,0,'bool','blue' ];
+// for( let i=0;i<arrayRandom.length;i++){
+//     console.log(arrayRandom[i]);
+// }
+
+//task 14-16
+// let arrayRandom=['try',32,true,98,false,-12,true,0,'bool','blue' ];
+// for( let i=0;i<arrayRandom.length;i++){
+//     if(typeof arrayRandom[i] == "boolean"){
+//         console.log(arrayRandom[i])
+//     }
+// }
+//
+// let arrayRandom1=['try',32,true,98,false,-12,true,0,'bool','blue' ];
+// for( let i=0;i<arrayRandom1.length;i++){
+//     if(typeof arrayRandom1[i] == "number"){
+//         console.log(arrayRandom1[i])
+//     }
+// }
+//
+// let arrayRandom2=['try',32,true,98,false,-12,true,0,'bool','blue' ];
+// for( let i=0;i<arrayRandom2.length;i++){
+//     if(typeof arrayRandom2[i] == "string"){
+//         console.log(arrayRandom2[i])
+//     }
+// }
+
+//task17
+// let array=[];
+// array[0]=2;
+// array[1]='hello';
+// array[2]=true;
+// array[3]=23;
+// array[4]='black';
+// array[5]=-12;
+// array[6]=false;
+// array[7]='car';
+// array[8]=true;
+// console.log(array)
+
+//task18
+// for(let i=0;i<10;i++){
+//     console.log(i);
+//     document.write(i+'\n');
+// }
+
+//19
+// for(let i=0;i<100;i++){
+//     console.log(i);
+//     document.write(i+'\n');
+// }
+
+//20
+//
+// for(let i=0;i<100;i=2+i){
+//     console.log(i);
+//     document.write(i+'\n');
+// }
+
+
+//21
+// for(let i=0;i<=100;i++){
+//     if(i%'2'===0){
+//         console.log(i);
+//         document.write(i+'\n');
+//     }
+// }
+
+//23
+// for(let i=0;i<=100;i++){
+//     if(i%'2'===1){
+//         console.log(i);
+//         document.write(i+'\n');
+//     }
+// }
+
+//24
+// for(let min=0;min<2;min++){
+//     for(let second=0;second<=60;second++){
+//         console.log(min+'min',second+'second')
+//     }
+// }
+
+//25
+// for(let h=0;h<=2;h++){
+//   for(let min=0;min<20;min++){
+//     for(let second=0;second<=60;second++){
+//         console.log(h+'h', min+'min',second+'second')
+//     }
+// }
+// }
