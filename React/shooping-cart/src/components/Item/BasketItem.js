27819603eba04684c9d594b4b26b959b1295ae01
Grafin425 <clavik.edin@gmail.com>
2020-12-30 14:16:56 +0200

    route add follow page,add elem loading,

--- React/shooping-cart/src/components/Item/BasketItem.js
+++ React/shooping-cart/src/components/Item/BasketItem.js
@@ -2,21 +2,22 @@ import plus from '../../image/plus.svg'
 import minus from '../../image/minus.svg'
 import trash from '../../image/trash.png'
 import {useDispatch} from "react-redux";
-import {removeItem,addItemQty,decrease} from '../../redux'
+import {removeItem, addItemQty, decrease} from '../../redux'
 
 export default function BasketItem({item}) {
     const dispatch = useDispatch();
-    const removeItemFunc=(item)=>{
+    const removeItemFunc = (item) => {
         dispatch(removeItem(item))
     }
-    const qtyItem=(item)=>{
+    const qtyItem = (item) => {
         dispatch(addItemQty(item))
     }
-    const minusItem=(item)=>{
+    const minusItem = (item) => {
         dispatch(decrease(item))
     }
 
-    const { title, price, image,quantity} = item
+
+    const {title, price, image, quantity} = item
     return (
         <card
         >
@@ -31,17 +32,18 @@ export default function BasketItem({item}) {
             </info>
             <p className='qty'> Qty:{quantity}</p>
 
-            <span style={{marginBottom:10}}>
-                {item.quantity>1 &&
-                    <img onClick={() => minusItem(item)} src={minus} style={{wight: 22, height: 22}}
-                         className='icon' alt=""/>
+            <span style={{marginBottom: 10}}>
+                {item.quantity > 1 &&
+                <img onClick={() => minusItem(item)} src={minus} style={{wight: 22, height: 22}}
+                     className='icon' alt=""/>
                 }
-                {item.quantity===1 &&
+                {item.quantity === 1 &&
                 <img onClick={() => removeItemFunc(item)} src={trash} style={{wight: 22, height: 22}}
                      className='icon' alt=""/>
                 }
 
-            <img onClick={()=>qtyItem(item)} className='icon' src={plus} style={{wight: 22, height: 22,marginLeft:10}} alt=""/>
+                <img onClick={() => qtyItem(item)} className='icon' src={plus}
+                     style={{wight: 22, height: 22, marginLeft: 10}} alt=""/>
             </span>
         </card>
     )
