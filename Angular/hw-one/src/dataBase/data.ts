203394d798f7412322bbe13f6a520febaf16fce9
Grafin425 <clavik.edin@gmail.com>
2021-01-20 14:03:27 +0200

    angular/homework-1

---
+++ Angular/hw-one/src/dataBase/data.ts
@@ -0,0 +1,13 @@
+export let cars = [
+  {model: 'wv', year: '2001', power: '210kv', color: 'grbaly', door: 3},
+  {model: 'opel', year: '20012', power: '100', color: 'balck', door: 5},
+  {model: 'wv', year: '2002', power: '231', color: 'red', door: 3},
+  {model: 'bmw', year: '2005', power: '70', color: 'orange', door: 3},
+  {model: 'tesla', year: '2019', power: '32', color: 'green', door: 5},
+  {model: 'mia', year: '2013', power: '123', color: 'whier', door: 3},
+  {model: 'ga', year: '2020', power: '344', color: 'pink', door: 5},
+  {model: 'mtz', year: '2011', power: '212', color: 'black', door: 5},
+  {model: 'audi', year: '2004', power: '90', color: 'yellow', door: 5},
+  {model: 'bsd', year: '2017', power: '56', color: 'grey', door: 3}
+];
+
