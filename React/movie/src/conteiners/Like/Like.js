7f84c020d802454d6cb7b64c9637aebcb615b458
Grafin425 <clavik.edin@gmail.com>
2021-01-23 01:51:00 +0200

    style home

--- React/movie/src/conteiners/Like/Like.js
+++ React/movie/src/conteiners/Like/Like.js
@@ -4,13 +4,10 @@ export const Like=()=>{
     const {wishlist} = useSelector(({wishlist: {wishlist}}) => ({
         wishlist
     }))
-    const wishlistStore = localStorage.getItem('wishlist');
-    console.log(wishlistStore);
     return(
       <div>
           {
-              wishlist && wishlist.map(item=><h1>{item.original_title}</h1>
-              )
+              wishlist&&wishlist.map(item=><h1>{item.original_title}</h1>)
           }
       </div>
   )
