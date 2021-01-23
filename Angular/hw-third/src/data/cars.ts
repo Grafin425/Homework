d9ac43b4968128be7d48624b0ccd42d989c7bab4
Grafin425 <clavik.edin@gmail.com>
2021-01-23 14:50:50 +0200

    hw-3/router

---
+++ Angular/hw-third/src/data/cars.ts
@@ -0,0 +1,12 @@
+export const carsdriver = [
+  {id: 0, model: 'wv', year: '2001', power: '210kv', color: 'grbaly', drive: 'vasiya'},
+  {id: 1, model: 'opel', year: '20012', power: '100', color: 'balck', drive: 'ivan'},
+  {id: 2, model: 'wv', year: '2002', power: '231', color: 'red', drive: 'roma'},
+  {id: 3, model: 'bmw', year: '2005', power: '70', color: 'orange', drive: 'kolya'},
+  {id: 4, model: 'tesla', year: '2019', power: '32', color: 'green', drive: 'vasya'},
+  {id: 5, model: 'mia', year: '2013', power: '123', color: 'whier', drive: 'andriy'},
+  {id: 6, model: 'ga', year: '2020', power: '344', color: 'pink', drive: 'pol'},
+  {id: 7, model: 'mtz', year: '2011', power: '212', color: 'black', drive: 'stetsham'},
+  {id: 8, model: 'audi', year: '2004', power: '90', color: 'yellow', drive: 'petro'},
+  {id: 9, model: 'siat', year: '2017', power: '56', color: 'grey', drive: 'misha'}
+];
